b70e2ce6c16c040c25337fd963c8eb226ba97f70
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-05 11:37:49 +0000

    add t2

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - inster a right node
+ * @parent: node actual
+ * @value: value for n in node
+ * Return: return node
+ */
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = value;
+
+	if (new->n == '\0')
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->left = NULL;
+	new->right = parent->right;
+
+	if (new->right != NULL)
+		new->right->parent = new;
+
+	parent->right = new;
+	new->parent = parent;
+
+	return (new);
+}
