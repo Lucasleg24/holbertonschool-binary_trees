ea89c48d7206b49cf120fea5347dc6141b003207
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-05 09:27:23 +0000

    add main branch t0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,18 +1,32 @@
 #include "binary_trees.h"
 
 /**
- *
+ * binary_tree_node - function for create node
+ * @parent: pointer to parent node
+ * @value: value of n in node
+ * Return: return a struct type
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t new;
-
-	if (parent == NULL)
-		return (NULL);
+	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
 		return (NULL);
 
+	new->n = value;
+
+	if (new->n == '\0')
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
+
+	return (new);
+}
