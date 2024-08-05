015d08f462299b9f66fb8895d909349de4afce80
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-05 11:32:56 +0000

    fixing files for task 1

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,24 +1,6 @@
 #include "binary_trees.h"
 
 /**
- *
- */
-
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t new;
-
-	if (parent == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(binary_tree_t));
-
-	if (new == NULL)
-		return (NULL);
-
-#include "binary_trees.h"
-
-/**
  * binary_tree_node - function for create node
  * @parent: pointer to parent node
  * @value: value of n in node
