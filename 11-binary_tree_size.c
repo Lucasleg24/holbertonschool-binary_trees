8e4ad86eff2d09ab49bdf1e4db0af4ef9cb75a8e
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-06 09:39:20 +0000

    adding files for task 11

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_size - measures the size of a binary tree
+* @tree: tree that we want to measure the size
+*
+* Return: 0 if NULL, else the size of the tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{}
