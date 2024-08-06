f7317f6a4dfb7d7e6a041ea81a7ba11df90342f3
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-06 07:44:41 +0000

    updating file 9 on main

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,15 +1,31 @@
-#include "binary_trees"
+#include "binary_trees.h"
 
 /**
 * binary_tree_height - measures the height of a binary tree
 * @tree: tree to measure the height
 *
-* Return:
+* Return: 0 if tree is NULL
  */
  size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree = NULL)
+	size_t count = 0;
+
+	if (tree == NULL)
 		return (0);
 
-	while ()
+	while (tree->left != NULL || tree->right != NULL)
+	{
+		if (tree->right != NULL)
+		{
+			tree = tree->right;
+			count++;
+		}
+
+		if (tree->left != NULL)
+		{
+			tree = tree->left;
+			count++;
+		}
+	}
+	return (count);
 }
