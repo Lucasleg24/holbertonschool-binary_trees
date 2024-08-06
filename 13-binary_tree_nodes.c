741c320581596b6c0a2063d73775f2db784e7a76
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-06 11:21:57 +0000

    updating file 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,4 +8,17 @@
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
-{}
+{
+	size_t count = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	binary_tree_nodes(tree->left);
+	binary_tree_nodes(tree->right);
+
+	if (tree-> left != NULL || tree->right != NULL)
+		count++;
+
+	return (count);
+}
