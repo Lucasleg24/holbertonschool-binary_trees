3a24265cd9c4c51099b3c749a8def7c0654c9bbb
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-06 12:14:47 +0000

    updating file 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,19 +8,14 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	int state = 0;
-
 	if (tree == NULL)
 		return(0);
 
-	binary_tree_is_full(tree->left);
-	binary_tree_is_full(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-	if ((tree->left == NULL && tree->right == NULL) || (tree->left != NULL && tree->right != NULL))
-		state++;
-
-	else 
-		state--;
-
-	return (state);
+	if (tree->left != NULL && tree->right != NULL)
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	
+	return (0);
 }
