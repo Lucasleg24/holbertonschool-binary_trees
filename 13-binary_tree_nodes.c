d893f816b41dfacd891115533721d5f1a7846ade
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-06 11:44:36 +0000

    correcting betty style for file 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -16,9 +16,9 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
-	
-	if (tree-> left != NULL || tree->right != NULL)
+
+	if (tree->left != NULL || tree->right != NULL)
 		count++;
-	
+
 	return (count);
 }
