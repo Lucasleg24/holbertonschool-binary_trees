ec739ff84bd15f8c36bfb511bc570662fa134ec5
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-06 09:57:57 +0000

    updating file 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -13,8 +13,8 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	lsize = binary_tree_size(tree->left) +1;
-	rsize = binary_tree_size(tree->right) +1;
+	lsize = binary_tree_size(tree->left);
+	rsize = binary_tree_size(tree->right);
 
 	size = 1 + lsize + rsize;
 	return (size);
