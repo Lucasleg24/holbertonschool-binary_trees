84497180a0a672cdcd61f844b2dc83beec51683b
Lucasleg24 <lucaslegrand.pro@gmail.com>
2024-08-06 08:20:19 +0000

    add t12 on main with correctif

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,17 +8,11 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL)
-		count++;
-	if (tree->right != NULL)
-		count++;
 	if (tree->left == NULL && tree->right == NULL)
-		count++;
+		return (1);
 
-	return (count);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
