a5fa972e3781d18cae5cde6a1e25e34901495288
pelcam <YObY0ntfWsNZdceSM8hJDU5GgA813o1VMm6A@github.com>
2024-08-06 09:52:53 +0000

    updating file 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,14 +2,20 @@
 
 /**
 * binary_tree_size - measures the size of a binary tree
-* @tree: tree that we want to measure the size
+* @tree: pointer to the root node of the tree to measure the size
 *
-* Return: 0 if NULL, else the size of the tree
+* Return: the size of the tree, or 0 if NULL
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	size_t size = 0, lsize = 0, rsize = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	
+	lsize = binary_tree_size(tree->left) +1;
+	rsize = binary_tree_size(tree->right) +1;
+
+	size = 1 + lsize + rsize;
+	return (size);
 }
